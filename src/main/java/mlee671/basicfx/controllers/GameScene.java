9d93218325f9206caed14c2ab3fd6d0bf8ad6c69
Mlee671 <mitchwill.lee@gmail.com>
2025-09-01 19:30:17 +1200

    Added a apple to Scene1 which you chase around to increase your score.

--- src/main/java/mlee671/basicfx/controllers/GameScene.java
+++ src/main/java/mlee671/basicfx/controllers/GameScene.java
@@ -3,11 +3,11 @@ package mlee671.basicfx.controllers;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
 import mlee671.basicfx.App;
-//import mlee671.basicfx.App;
 
 public class GameScene {
 
@@ -18,17 +18,17 @@ public class GameScene {
     @FXML private GridPane gridArea;
     @FXML private Button btnMenu;
     @FXML private Button btnExit;
-    
-    private int playerRow = 0;
-    private int playerCol = 0;
+    @FXML private ImageView imageApple;
+
+    private int[] pCoord = {0, 0};
+    private int[] imCoord = {2, 2};
+    private int score = 0;
 
     @FXML
     private void initialize() {
-        // Initialization logic for the secondary controller
-        System.out.println("SecondaryController initialized");
         // Place the player at the starting position
-        GridPane.setRowIndex(iconPlayer, playerRow);
-        GridPane.setColumnIndex(iconPlayer, playerCol);
+        GridPane.setRowIndex(iconPlayer, pCoord[0]);
+        GridPane.setColumnIndex(iconPlayer, pCoord[1]);
     }
 
     @FXML
@@ -36,33 +36,59 @@ public class GameScene {
         // Handle key press events
         switch (event.getCode()) {
             case W:
-                if (playerRow > 0) playerRow--;
-                System.out.println("Moving Up");
+                if (pCoord[0] > 0) pCoord[0]--;
                 break;
             case S:
-                if (playerRow < 5) {
-                    playerRow++;
+                if (pCoord[0] < 5) {
+                    pCoord[0]++;
                 }
-                System.out.println("Moving Down");
                 break;
             case A:
-                if (playerCol > 0) {
-                    playerCol--;
+                if (pCoord[1] > 0) {
+                    pCoord[1]--;
                 }
-                System.out.println("Moving Left");
                 break;
             case D:
-                if (playerCol < 5) {
-                    playerCol++;
+                if (pCoord[1] < 5) {
+                    pCoord[1]++;
                 }
-                System.out.println("Moving Right");
                 break;
             default:
                 break;
         }
         // Move the player icon
-        GridPane.setRowIndex(iconPlayer, playerRow);
-        GridPane.setColumnIndex(iconPlayer, playerCol);
+        GridPane.setRowIndex(iconPlayer, pCoord[0]);
+        GridPane.setColumnIndex(iconPlayer, pCoord[1]);
+
+        // check if Apple
+        if (playerOnItem()) {
+            System.out.println("You found an apple!");
+            // Move the apple to a new random position
+            moveItemRandomly();
+            increaseScore();
+        }
+
+    }
+
+    private boolean playerOnItem() {
+        return pCoord[0] == imCoord[0] && pCoord[1] == imCoord[1];
+    }
+
+    private void moveItemRandomly() {
+        imCoord[0] = (int) (Math.random() * 5);
+        imCoord[1] = (int) (Math.random() * 5);
+        if (playerOnItem()) {
+            moveItemRandomly();
+            return;
+        }
+        GridPane.setRowIndex(imageApple, imCoord[0]);
+        GridPane.setColumnIndex(imageApple, imCoord[1]);
+    }
+
+    private void increaseScore() {
+        // Logic to increase the player's score
+        score++;
+        System.out.println("Score increased! Current score: " + score);
     }
 
     @FXML
